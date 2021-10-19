ed95b0e66baecfecdec03cfec0e2aa8aeb761842
HauntedHunteer <m.niemczuk@interia.pl>
2021-10-19 23:23:09 +0200

    Initial commit

---
+++ src/main/java/com/niemczuk/passwordwallet/HelloController.java
@@ -0,0 +1,12 @@
+package com.niemczuk.passwordwallet;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+    @GetMapping("/hello")
+    public String getHello() {
+        return "hello";
+    }
+}
