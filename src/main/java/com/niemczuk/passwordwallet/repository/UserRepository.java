15907fe8f63907ad230040e0099002efe305790c
HauntedHunteer <m.niemczuk@interia.pl>
2021-10-22 10:28:20 +0200

    User repository updated

--- src/main/java/com/niemczuk/passwordwallet/repository/UserRepository.java
+++ src/main/java/com/niemczuk/passwordwallet/repository/UserRepository.java
@@ -8,5 +8,8 @@ import java.util.UUID;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, UUID> {
+
+    boolean existsByLogin(String login);
+
     User findByLogin(String login);
 }
