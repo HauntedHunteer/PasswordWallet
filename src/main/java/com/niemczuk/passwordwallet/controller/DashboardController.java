1b2d5592623dd99a6de513b2129055171aff934a
HauntedHunteer <m.niemczuk@interia.pl>
2021-10-24 20:46:37 +0200

    Change passwd logout fix

--- src/main/java/com/niemczuk/passwordwallet/controller/DashboardController.java
+++ src/main/java/com/niemczuk/passwordwallet/controller/DashboardController.java
@@ -8,6 +8,7 @@ import com.niemczuk.passwordwallet.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,7 +16,8 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 
-
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 
 @Controller
@@ -65,8 +67,13 @@ public class DashboardController {
     }
 
     @PostMapping("/changePassword")
-    public String changePassword(@ModelAttribute @Valid ChangePasswordDto changePasswordDto) throws Exception {
+    public String changePassword(@ModelAttribute @Valid ChangePasswordDto changePasswordDto, HttpServletRequest request, HttpServletResponse response) throws Exception {
         passwordService.changeUserPassword(changePasswordDto);
-        return "redirect:/signIn";
+
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        if (auth != null){
+            new SecurityContextLogoutHandler().logout(request, response, auth);
+        }
+        return "redirect:/login?logout";
     }
 }
