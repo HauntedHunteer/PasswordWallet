ed7328e03e34c87c8eb32049d97bc6c8e8f50751
HauntedHunteer <m.niemczuk@interia.pl>
2022-01-19 12:56:16 +0100

    Share template fix

--- src/main/resources/static/js/util-functions.js
+++ src/main/resources/static/js/util-functions.js
@@ -3,7 +3,10 @@ window.onload = function () {
 }
 
 
-function showOrHidePasswordForm() {
+function showShareForm(passwordId, websiteName) {
     let form = document.getElementById("shareForm");
-    form.style.display = (form.style.display === 'none') ? 'block' : 'none';
+    //form.style.display = (form.style.display === 'none') ? 'block' : 'none';
+    document.getElementById("passId").value = passwordId;
+    document.getElementById("websiteName").textContent = websiteName;
+    form.style.display = 'block';
 }
